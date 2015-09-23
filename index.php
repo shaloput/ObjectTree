f0b23c35a89928194deaedde8353c8eaacbe974f
Sergey Shnepp <oceanserg@yandex.ru>
2015-09-23 22:16:06 +0300

    Before recurison

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,11 +12,17 @@
 		<div class="container">
 			<h3>Object Manager</h2>
 		
-		<form class="login" method="post" action="login.php">
-			<input type="text" name="username">
-			<input type="password" name="password">
-			<input class="btn" type="Submit" value="Log In">
-		</form>
+		<?php 
+			if (!isset($_SESSION['uid'])) {
+				echo "<form class='login' method='post' action='login_parse.php'>
+					<input type='text' name='username'>
+					<input type='password' name='password'>
+					<input class='btn' type='Submit' value='Log In'>
+				</form>";
+			} else {
+				echo "<p class='login'>You are logged in as ".$_SESSION['username']." &bull; <a href='logout_parse.php'>Logout</a>";
+			}
+		?>
 			
 		</div>
 	</div>
@@ -24,7 +31,30 @@
 		<h1>Welcome to Object Tree Manager</h1>
 
 		<div class="contents">
-			
+		<h3>Objects:</h2>
+			<div class="obj-tree">
+			 <?php 
+				include_once("connect.php");
+				$sql = "SELECT * FROM objects WHERE parent_id=0	 ORDER BY obj_id ASC";
+
+				$res = mysql_query($sql) or die(mysql_error());
+				$objects = "";
+
+				if (mysql_num_rows($res) > 0) {
+					while ($row = mysql_fetch_assoc($res)) {
+						$id = $row['obj_id'];
+						$name = $row['name'];
+						$descr = $row['descr'];
+						$objects .= "<p>".$name."</p>";
+					}
+
+					echo $objects;
+				} else {
+					echo "<p>There are no object avaible yet.</p>"; 
+				}
+
+			 ?>
+			</div>
 		</div>
 	</div>
 			
---
+++ login_parse.php
@@ -0,0 +1,26 @@
+<?php 
+
+session_start();
+include_once("connect.php");
+
+if (isset($_POST['username'])) {
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$sql = "SELECT * FROM users WHERE username='".$username."' AND password='".$password."' LIMIT 1";
+	$res = mysql_query($sql) or die(mysql_error());
+
+	if (mysql_num_rows($res) == 1) {
+		$row = mysql_fetch_assoc($res);
+		$_SESSION['uid'] = $row ['id'];
+		$_SESSION['username'] = $row ['username'];
+		header("Location: index.php");
+		exit();
+	} else {
+		echo "Invalid login information. Please return to th previous page";
+		exit();
+	}
+}
+
+?>
